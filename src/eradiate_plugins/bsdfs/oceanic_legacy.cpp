339b59773e28a7f8d8f1a3c2d826cba88ea4710f
Matthias <rocketpencil.studios@gmail.com>
2024-07-10 15:18:14 +0200

    Implement evaluation for whitecap reflectance

--- src/eradiate_plugins/bsdfs/oceanic_legacy.cpp
+++ src/eradiate_plugins/bsdfs/oceanic_legacy.cpp
@@ -14,7 +14,8 @@ public:
 
     OceanicBSDF(const Properties &props) : Base(props) {
         // Retrieve the parameters used in 6SV
-        //m_wind_speed = props.texture<Texture>("wind_speed");
+        m_wavelength = props.texture<Texture>("wavelength");
+        m_wind_speed = props.texture<Texture>("wind_speed");
 
         // Set the BSDF flags
         // => Whitecap reflectance is "diffuse"
@@ -90,7 +91,6 @@ public:
 
     Spectrum eval(const BSDFContext &ctx, const SurfaceInteraction3f &si,
                   const Vector3f &wo, Mask active) const override {
-        /*
         MI_MASKED_FUNCTION(ProfilerPhase::BSDFEvaluate, active);
 
         bool has_whitecap = ctx.is_enabled(BSDFFlags::DiffuseReflection, 0);
@@ -100,22 +100,34 @@ public:
         Float cos_theta_i = Frame3f::cos_theta(si.wi),
               cos_theta_o = Frame3f::cos_theta(wo);
 
+        // Ensure incoming and outgoing directions are in the upper hemisphere
+        active &= cos_theta_i > 0.f && cos_theta_o > 0.f;
+
         // Compute the whitecap reflectance
         UnpolarizedSpectrum result(0.f);
 
         if (has_whitecap) {
             UnpolarizedSpectrum wind_speed = m_wind_speed->eval(si, active);
-
+            
             // Koepke 1984 with linear interpolation to obtain the efficiency factor
             // The maximum wind speed is chosen to be 38 m/s as to not exceed fractional coverage limits
-            result[active] = 4.0f + [4.0f * (wind_speed / 38.0f) - 2.0f];
+            auto wind_speed_frac = wind_speed / m_wind_speed_max;
+            auto f_eff = m_f_eff_base + (m_f_eff_base * wind_speed_frac - m_f_eff_mod);
+
+            // Here we compute the effective reflectance by looking up the tabulated
+            // values provided by Whitlock et al. 1982
+            auto index_f = (m_wavelength->eval(si, active).x() - Float(0.2)) / Float(0.1);
+            auto index_i = dr::floor2int<UInt32>(index_f);
+            auto eff_reflectance = dr::gather<Float>(m_eff_reflectance, index_i);
+
+            // Based on the power-law provided by Monahan & Muircheartaigh 1980,
+            // the fractional whitecap coverage can be determined
+            auto coverage = m_monahan_alpha * dr::pow(wind_speed, m_monahan_lambda);
+
+            result[active] = coverage * f_eff * eff_reflectance;
         }
 
-        // TODO: Multiply by the cosine term???
-
-        return dr::select(active, result, 0.f);
-        */
-       return dr::select(active, UnpolarizedSpectrum(1.f), 0.f);
+        return depolarizer<Spectrum>(result) & active;
     }
 
     Float pdf(const BSDFContext &ctx, const SurfaceInteraction3f &si,
@@ -143,17 +155,32 @@ public:
 
     std::string to_string() const override {
         std::ostringstream oss;
+        oss << "OceanicLegacy[" << std::endl
+            << std::endl
+            << "]";
         return oss.str();
     }
 
     MI_DECLARE_CLASS()
 private:
-    // Paremeters to compute fractional whitecap coverage based on 
-    // "optimal power-law descrption of oceanic whitecap coverage 
-    // dependence on wind speed" by Monahan et al.
-    //ref<Texture> m_wind_speed;
-    //float m_monahan_alpha = 2.951 * 10e-6;
-    //float m_monahan_lambda = 3.52;
+    // User-provided parameters
+    ref<Texture> m_wavelength;
+    ref<Texture> m_wind_speed;
+
+    // Parameters used to compute whitecap reflectance
+    ScalarFloat m_f_eff_base = 0.4f;
+    ScalarFloat m_f_eff_mod = 0.2f;
+    ScalarFloat m_wind_speed_max = 38.0f;
+    ScalarFloat m_monahan_alpha = 2.951 * 10e-6;
+    ScalarFloat m_monahan_lambda = 3.52;
+
+    // These values range from 0.2 to 4.0 μm
+    ScalarFloat m_eff_reflectance[39] = {
+        0.220, 0.220, 0.220, 0.220, 0.220, 0.220, 0.215, 0.210, 0.200, 0.190,
+        0.175, 0.155, 0.130, 0.080, 0.100, 0.105, 0.100, 0.080, 0.045, 0.055,
+        0.065, 0.060, 0.055, 0.040, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000,
+        0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000
+    };
 };
 
 MI_IMPLEMENT_CLASS_VARIANT(OceanicBSDF, BSDF)
